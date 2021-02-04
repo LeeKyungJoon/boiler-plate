7ec56a654e001add60d66f679b4ab710784894ba
MoonLight <dlsoftedev02@gmail.com>
2021-02-04 21:29:11 +0900

    ***token, auth, logout

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -14,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxLength: 10
+        maxLength: 100
     },
     lastname: {
         type: String,
@@ -49,10 +50,48 @@ userSchema.pre('save', function(next) {
                 next()
             })
         })
+    } else {
+        next()
     }
     
     
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword - kkkk789  암호화된 비번 - $2b$10$45FOZDAhBcorGym7E4aBvOLoHmYyIQkui8.XO89N6Kcp/UoOAbcya
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    
+    var user = this;
+    
+    //jsonwebtoken을 이용해서 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this
+    //토큰 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 후
+        //클라에서 가져온 token과 db에 보관된 token이 일치하는지 확인
+
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
